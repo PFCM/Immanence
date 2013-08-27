04a8857c6e56ea2054325bfb361ed2121318dc03
timothybarraclough <barraclough.tim@gmail.com>
2013-08-27 15:50:53 +1200

    Added the Sensor code for the SF box

--- Arduino/Sensors/Sensors.ino
+++ Arduino/Sensors/Sensors.ino
@@ -1,1 +1,28 @@
-// This will be the sensor Input code that I will write - TJB
+int ultrasound = A15;
+int infrared = A14;
+
+
+
+int readings[] = {0,0};
+
+
+void setup(){
+ 
+  
+ Serial.begin(9600);
+  
+}
+
+void loop(){
+ 
+ readings[0] = analogRead(ultrasound);
+ readings[1] = analogRead(infrared);
+ 
+ Serial.print(readings[0]);
+ Serial.print("  /  ");
+ Serial.println(readings[1]);
+ 
+ delay (100);
+ 
+  
+}
