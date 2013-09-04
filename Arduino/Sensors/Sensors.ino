6bf097e151582939162aeb566c2c46ec9d5a7529
timothybarraclough <barraclough.tim@gmail.com>
2013-09-04 14:30:57 +1200

    Serial communication with Chuck and Arduino half sorted, still not receiving data out of chuck, but we can sort that...........

--- Arduino/Sensors/Sensors.ino
+++ Arduino/Sensors/Sensors.ino
@@ -13,8 +13,9 @@ const int SPEEDUP = 3;
 int currentstate = SPEEDUP;
 
 //READINGS ARE US/IR/Pressure
+int readingsize = 3; //Change this once the pressure has been attached
 int readings[] = {
-  0,0,0};
+  0,0,0,0};
 
 
 void setup(){
@@ -24,19 +25,13 @@ void setup(){
 }
 
 void loop(){
-
-  readings[0] = map(analogRead(ultrasound),1023,0,0,127);
-  readings[1] = map(analogRead(infrared),1023,0,0,127); 
-  readings[2] = map(analogRead(pressure),0,1024,0,127);
+  readings[0] = currentstate;
+  readings[1] = analogRead(ultrasound); //map(analogRead(ultrasound),1023,0,0,127);
+  readings[2] = analogRead(infrared); //map(analogRead(infrared),1023,0,0,127); 
+  readings[3] = map(analogRead(pressure),0,1024,0,127);
 
   //Tim's super Proto-cool
-  Serial.print(200);
-  Serial.print(readings[0]);
-  Serial.print(205);
-  Serial.print(readings[1]);
-  Serial.print(210);
-  Serial.print(readings[2]);
-  Serial.print(215);
+serialPrint(readings);
   //Serial.print(EncoderPostion) as (DEGREES/3)
 
 
@@ -80,5 +75,23 @@ void loop(){
   }
 
   delay (10);
+}
+
+void serialPrint(int x[]){
+  //Start with a left brace
+  Serial.print("[");
+  
+  //Print all the characters within the array (except the last one), followed by a comma
+  for (int i = 0; i < readingsize-1; i++)
+  {
+    Serial.print(x[i]);
+    Serial.print(",");
+  }
+  //Print the last character
+  Serial.print(x[readingsize-1]);
+  //Right Brace
+  Serial.print("]");
+  //End Line
+  Serial.print("\n");
 }
 
